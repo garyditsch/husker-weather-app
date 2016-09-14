0057891692f0253be936174ec1fed3260d440760
Gary Ditsch <garyditsch@gmail.com>
2016-09-14 11:59:40 -0400

    adding styes to current weather

--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@ from flask import render_template, request
 
 from app import app
 from app.forms import WeatherForm
+from app.open_weather_api import OpenWeatherAPI
 
 @app.route("/")
 @app.route("/index")
@@ -11,14 +12,19 @@ def index():
 @app.route("/current", methods=["GET", "POST"])
 def current_weather():
     weather_form = WeatherForm(request.form)
+    weather_item = None
 
     if request.method == "POST" and weather_form.validate():
         #do stuff with the valid form
-        pass
+        city = weather_form.city.data
+        country_code = weather_form.country_code.data
+
+        api = OpenWeatherAPI(app.config["OPEN_WEATHER_API_KEY"])
+        weather_item = api.get_current_weather(city, country_code)
 
 
     return render_template("current.html", 
-        weather_form=weather_form)
+        weather_form=weather_form, weather_item=weather_item)
 
 @app.route("/forecast", methods=["GET", "POST"])
 def forecast_weather():
--- run.py
+++ run.py
@@ -1,3 +1,13 @@
+import sys
+
+
 from app import app
 
+try:
+    import settings
+except ImportError:
+    print("Please copy settings-dist.py and update your API key")
+    sys.exit
+
+app.config.from_object(settings)
 app.run(debug=True)
